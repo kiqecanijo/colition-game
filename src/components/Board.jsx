a2d79fe2a2846d5dda7e8746bcf93fdf48ec979a
kiqecanijo <enry1375@gmail.com>
2019-05-31 16:52:01 -0500

    changed board to blue

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from 'react'
 
 const style = (dimension) => {
-    const dim = dimension + 'px';
-    return {
-        width: dim,
-        height: dim,
-        position: 'relative',
-        margin: '25px auto',
-        overflow: 'hidden',
-        borderRadius: '5px',
-        backgroundColor:'black',
-        border: '10px solid red',
-        borderRadius: '10px',
-        boxShadow: '0px 0px 30px red'
+  const dim = dimension + 'px'
+  return {
+    width: dim,
+    height: dim,
+    position: 'relative',
+    margin: '25px auto',
+    overflow: 'hidden',
+    borderRadius: '5px',
+    backgroundColor: 'black',
+    border: '10px solid blue',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 30px blue'
 
-    };
-};
+  }
+}
 
 export default ({ dimension, children }) => (
-    <div style={style(dimension)}>
-        {children}
+  <div style={style(dimension)}>
+    {children}
 
-    </div>
+  </div>
 )
