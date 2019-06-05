cc429908cb0a2d8d697b33b8ccba56bf7c5d1cb7
kiqecanijo <enry1375@gmail.com>
2019-06-05 16:53:22 -0500

    copied top component

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,31 +1,92 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import jaguar from '../sprites/jaguar-score.png'
+
 const Div = styled.div`
-color:white
+ max-width: 800px;
+ text-align:center;
+ margin:auto;
+ color:white;
+font-family:nunito
+`
+const You = styled.div`
+display:flex;
+font-size: 30px;
+font-weight: 800;
+text-align:center;
+
+`
+const Thanks = styled.h1`
+font-size: 50px;
+font-weight: 900;
+text-align:center
+`
+const YouScore = styled.h1`
+font-size: 30px;
+font-weight: 900;
+text-align:left;
+color:#d49a18;
+`
+
+const Td = styled.td`
+  text-align:center;
+  font-size:24px;
+  width: 200px;
+`
+const Th = styled(Td)
+
+const Img = styled.img`
+  width: 60px;
+  box-shadow:0px 0px 30px #0798ff;
+  border-radius: 10%;
+`
+const Mid = styled.div`
+  width: ${/* window.innerWidth < 600 ? 100 : 50 */50}%;
+  display: block;
+  text-align:center;
+  position:relative
 `
 
 const Top = props => {
-
-
-const {user,top} = props
-
-console.log(top);
-  return(
+  return (
     <Div>
-      your name: {user.name}
-      <br/>
-      your score: {user.score}
-      {top.map(usr => {
-        return(
-          <div>
-              <img src={usr.photo} />
-              <p>{usr.name}</p>
-              <p>{usr.score}</p>
+      <You>
+        <Mid>
+          <img src={jaguar}/>
+        </Mid>
+        <Mid>
+          <Thanks>
+        ¡Gracias por jugar!
+            <YouScore>
+            Tus<br/>Puntaje: {props.user.score}
+              <br/>
+            Tu<br/>actual posición : {props.top.filter(el => el.score > 0 && el.start_time > 0).map(el => el.name).indexOf(props.user.name) + 1}
+            </YouScore>
+          </Thanks>
+        </Mid>
 
-          </div>
-        )
-      })}
+      </You>
+
+      <Thanks>¡Las puntuaciones más altas!</Thanks>
+      <table>
+        <tbody>
+          <tr>
+            <th>posición </th>
+            <th> </th>
+            <th>Nombre </th>
+            <th > Puntaje </th>
+          </tr>
+          {props.top.filter(el => el.score > 0 && el.start_time > 0).map((user, index) =>
+            <tr>
+              <Td style={{ color: index == 0 && 'gold' || index == 1 && 'silver' || index == 2 && 'orange' }} > {index + 1 }</Td>
+              <Td colspan="2"> <Img src={user.photo}/></Td>
+              <Td colspan="2">{user.name}</Td>
+              <Td >{user.score}</Td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </Div>
   )
 }
